97743d9d33cbf2cc492ab1f084b54543a9adbc9c
Adebola Omoboya <adeomoboya@gmail.com>
2020-03-28 20:49:52 +0100

    The First Cut of the Project

---
+++ src/main/java/io/factorialsystems/fotojugbackend/FotojugBackendApplication.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.fotojugbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotojugBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FotojugBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/factorialsystems/fotojugbackend/FotojugBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.fotojugbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotojugBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
